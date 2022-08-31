46d8aaeeb09ef16934010dd05125efc2e7b154f8
ashigor <ashigor@gmail.com>
2022-09-01 01:44:10 +0300

    Исправила

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import statistics
+
 class Student():
     def __init__(self, name, surname, gender):
         self.name = name
@@ -23,19 +25,47 @@ class Student():
         return f'Имя: {self.name}, фамилия: {self.surname}, обучается на курсе: ' \
                f'{courses_in_progress}, cпиcок оценок: {ratings};'
 
-    def calculation_mean(self):
-        ratings = list(self.grades.values())
-        average_rating_student = sum(ratings[0]) / len(ratings[0])
-        return average_rating_student
+    # def calculation_mean(self):
+    #     ratings = list(self.grades.values())
+    #     average_rating_student = sum(ratings[0]) / len(ratings[0])
+    #     return average_rating_student
+
+    def __lt__(self, other):
+        # module_1 = calculation_mean(self.grades)
+        # module_2 = calculation_mean(other.grades)
+        # return module_1 < module_2
+        if isinstance(self, Student) and isinstance(other, Student):
+            return calculation_mean(self.grades) < calculation_mean(other.grades)
+        else:
+            return 'Ошибка!'
+
+    def __gt__(self, other):
+        # module_1 = calculation_mean(self.grades)
+        # module_2 = calculation_mean(other.grades)
+        # return module_1 > module_2
+        if isinstance(self, Student) and isinstance(other, Student):
+            return calculation_mean(self.grades) > calculation_mean(other.grades)
+        else:
+            return 'Ошибка!'
+
+    def __eq__(self, other):
+        # module_1 = calculation_mean(self.grades)
+        # module_2 = calculation_mean(other.grades)
+        # return module_1 == module_2
+        if isinstance(self, Student) and isinstance(other, Student):
+            return calculation_mean(self.grades) == calculation_mean(other.grades)
+        else:
+            return 'Ошибка!'
 
     def __str__(self):
         courses_in_progress = ", ".join(self.courses_in_progress)
         finished_courses = ", ".join(self.finished_courses)
         return f"Имя: {self.name}" \
                f"\nФамилия: {self.surname}" \
-               f"\nСредняя оценка за домашние задания: {self.calculation_mean()}" \
+               f"\nСредняя оценка за домашние задания: {calculation_mean(self.grades)}" \
                f"\nКурсы в процессе изучения: {courses_in_progress}" \
                f"\nЗавершенные курсы: {finished_courses}"
+
 
 # Задание № 1
 class Mentor:
@@ -57,14 +87,41 @@ class Lecturer(Mentor):
         return f'Имя: {self.name}, фамилия: {self.surname}, преподает: ' \
                f'{courses_in_progress}, оценки студентов: {ratings};'
 
-    def calculation_mean(self):
-        ratings = list(self.grades.values())
-        return sum(ratings[0]) / len(ratings[0])
+    # def calculation_mean(self):
+    #     ratings = list(self.grades.values())
+    #     return sum(ratings[0]) / len(ratings[0])
+
+    def __lt__(self, other):
+        # module_1 = calculation_mean(self.grades)
+        # module_2 = other.calculation_mean()
+        # return module_1 < module_2
+        if isinstance(self, Lecturer) and isinstance(other, Lecturer):
+            return calculation_mean(self.grades) < calculation_mean(other.grades)
+        else:
+            return 'Ошибка!'
+
+    def __gt__(self, other):
+        # module_1 = calculation_mean(self.grades)
+        # module_2 = other.calculation_mean()
+        # return module_1 > module_2
+        if isinstance(self, Lecturer) and isinstance(other, Lecturer):
+            return calculation_mean(self.grades) > calculation_mean(other.grades)
+        else:
+            return 'Ошибка!'
+
+    def __eq__(self, other):
+        # module_1 = calculation_mean(self.grades)
+        # module_2 = other.calculation_mean()
+        # return module_1 == module_2
+        if isinstance(self, Lecturer) and isinstance(other, Lecturer):
+            return calculation_mean(self.grades) == calculation_mean(other.grades)
+        else:
+            return 'Ошибка!'
 
     def __str__(self):
         return f"Имя: {self.name}" \
                f"\nФамилия: {self.surname}" \
-               f"\nСредняя оценка за лекции: {self.calculation_mean()}"
+               f"\nСредняя оценка за лекции: {calculation_mean(self.grades)}"
 
 
 class Reviewer(Mentor):
@@ -85,38 +142,81 @@ class Reviewer(Mentor):
         return f"Имя: {self.name}" \
                f"\nФамилия: {self.surname}"
 
+# Студент 1
+student_1 = Student('Ruoy', 'Eman', 'woman')
+student_1.courses_in_progress += ['Python', 'Git']
+student_1.finished_courses += ['Введение в программирование']
 
-some_student = Student('Ruoy', 'Eman', 'your_gender')
-some_student.courses_in_progress += ['Python', 'Git']
-some_student.finished_courses += ['Введение в программирование']
+# Студент 2
+student_2 = Student('Adam', 'Stander', 'male')
+student_2.courses_in_progress += ['Python', 'Git']
+student_2.finished_courses += ['Введение в программирование']
 
-some_reviewer = Reviewer('Some', 'Buddy')
-some_reviewer.courses_attached += ['Python', 'Git']
+# Студент 3
+student_3 = Student('Riki', 'Misumi', 'women')
+student_3.courses_in_progress += ['Python']
+student_3.finished_courses += ['Введение в программирование']
 
-some_lecturer = Lecturer('Some', 'Buddy')
-some_lecturer.courses_attached += ['Python', 'Git']
+# Эксперт
+reviewer_1 = Reviewer('Some', 'Buddy')
+reviewer_1.courses_attached += ['Python', 'Git']
 
-some_reviewer.rate_hw(some_student, 'Python', 10)
-some_reviewer.rate_hw(some_student, 'Python', 10)
-some_reviewer.rate_hw(some_student, 'Git', 10)
+# Лектор 1
+lecturer_1 = Lecturer('Robert', 'Steffen')
+lecturer_1.courses_attached += ['Python', 'Git']
 
-some_student.rate_hw(some_lecturer, 'Python', 9)
-some_student.rate_hw(some_lecturer, 'Python', 9)
-some_student.rate_hw(some_lecturer, 'Git', 9)
+# Лектор 2
+lecturer_2 = Lecturer('David', 'Miller')
+lecturer_2.courses_attached += ['Python', 'Git']
 
-# Задание № 3.1
-print(some_student)
-# print(some_lecturer)
-# print(some_reviewer)
+# Лектор 3
+lecturer_3 = Lecturer('Some', 'Buddy')
+lecturer_3.courses_attached += ['Python', 'Git']
 
-# Задание № 3.2
-# if some_lecturer.calculation_mean() > some_student.calculation_mean():
-#     print("Лекторов по средней оценке за лекции больше, чем студентов по средней оценке за домашние задания")
-# elif some_lecturer.calculation_mean() < some_student.calculation_mean():
-#     print("Студентов по средней оценке за домашние задания больше, чем лекторов по средней оценке за лекциии")
-# else:
-#     print("Лекторов по средней оценке за лекции равен студентов по средней оценке за домашние задания")
+# Эксперт ставит оценку к студенту 1
+reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 9)
+reviewer_1.rate_hw(student_1, 'Git', 10)
+
+# Эксперт ставит оценку к студенту 2
+reviewer_1.rate_hw(student_2, 'Python', 8)
+reviewer_1.rate_hw(student_2, 'Python', 9)
+reviewer_1.rate_hw(student_2, 'Git', 10)
+
+# Эксперт ставит оценку к студенту 3
+reviewer_1.rate_hw(student_3, 'Python', 8)
+reviewer_1.rate_hw(student_3, 'Python', 9)
+reviewer_1.rate_hw(student_3, 'Git', 10)
+
+# Студент 1 ставит оценку к лектору 1-3
+student_1.rate_hw(lecturer_1, 'Python', 9)
+student_1.rate_hw(lecturer_2, 'Python', 8)
+student_1.rate_hw(lecturer_3, 'Git', 7)
+
+# Студент 2 ставит оценку к лектору 1-3
+student_2.rate_hw(lecturer_1, 'Python', 8)
+student_2.rate_hw(lecturer_2, 'Python', 10)
+student_2.rate_hw(lecturer_3, 'Git', 9)
+
+# Студент 3 ставит оценку к лектору 1-3
+student_3.rate_hw(lecturer_1, 'Python', 8)
+student_3.rate_hw(lecturer_2, 'Python', 9)
+student_3.rate_hw(lecturer_3, 'Git', 9)
+
 
 # Задание № 4
-# print(some_student.description())
-# print(some_lecturer.description())
+def calculation_mean(grades):
+    ratings = list(grades.values())
+    average_rating_student = sum(ratings[0]) / len(ratings[0])
+    return average_rating_student
+
+
+# Задание № 3.1
+# print(student_1)
+# print(lecturer_1)
+# print(reviewer_1)
+
+
+# Задание № 3.2
+print(student_1 > student_2)
+# print(lecturer_1 > lecturer_2)
